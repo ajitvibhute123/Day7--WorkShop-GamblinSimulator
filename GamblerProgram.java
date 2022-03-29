2fec8450399ac54c91b85425de02ae6c5aec4991
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-30 01:02:21 +0530

    Added : Game started

---
+++ GamblerProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class GamblerProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambler Program");
+        int startingTotalAmount=100;
+        int betsPerGame=1;
+    }
+}
